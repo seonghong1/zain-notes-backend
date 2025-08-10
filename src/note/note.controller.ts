5c3e69614f4098f14d91de889bed9f409a64018e
seonghong1 <wodeanniversary@naver.com>
2025-08-10 23:43:16 +0900

    notes영역 작업

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,21 +1,70 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { NoteService } from './note.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { NoteDto } from './dto/note.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UpdateNoteDto } from './dto/update-note.dto';
+import { User } from 'src/auth/user.decorator';
 
 @Controller('notes')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @Get('/')
-  getNotes(): Promise<NoteDto[]> {
-    const notes = this.noteService.findAll();
-
+  @UseGuards(AuthGuard)
+  findAll(
+    @User('sub') userId: number,
+    @Query('date') date?: string,
+  ): Promise<NoteDto[]> {
+    const notes = this.noteService.findAll(userId, { date });
     return notes;
   }
 
+  @Get('/:id')
+  @UseGuards(AuthGuard)
+  findById(
+    @User('sub') userId: number,
+    @Param('id') id: number,
+  ): Promise<NoteDto> {
+    console.log(id);
+    return this.noteService.findById(userId, id);
+  }
+
   @Post('/')
-  createNote(@Body() noteData: CreateNoteDto): Promise<number> {
-    return this.noteService.create(noteData);
+  @UseGuards(AuthGuard)
+  create(
+    @Body() noteData: CreateNoteDto,
+    @User('sub') userId: number,
+  ): Promise<NoteDto> {
+    return this.noteService.create(noteData, userId);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AuthGuard)
+  update(
+    @Param('id') id: number,
+    @Body() noteData: UpdateNoteDto,
+    @User('sub') userId: number,
+  ): Promise<NoteDto> {
+    return this.noteService.update(id, noteData, userId);
+  }
+
+  @Delete('/:id')
+  @UseGuards(AuthGuard)
+  delete(
+    @Param('id') id: number,
+    @User('sub') userId: number,
+  ): Promise<number> {
+    return this.noteService.delete(id, userId);
   }
 }
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Raw, Repository } from 'typeorm';
 
 import { Note } from './entities/note.entity';
 import { NoteDto } from './dto/note.dto';
 import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Injectable()
 export class NoteService {
@@ -13,13 +14,84 @@ export class NoteService {
     private noteRepository: Repository<Note>,
   ) {}
 
-  async findAll(): Promise<NoteDto[]> {
-    const notes = await this.noteRepository.find();
+  async findAll(userId: number, query?: { date?: string }): Promise<NoteDto[]> {
+    const whereCondition: any = {
+      isDeleted: false,
+      userId,
+    };
+
+    if (query?.date) {
+      const startDate = new Date(query.date);
+      startDate.setHours(0, 0, 0, 0);
+
+      const endDate = new Date(query.date);
+      endDate.setHours(23, 59, 59, 999);
+
+      whereCondition.createdAt = Raw(
+        (alias) =>
+          `${alias} AT TIME ZONE 'UTC' AT TIME ZONE 'Asia/Seoul' BETWEEN :startDate AND :endDate`,
+        { startDate, endDate },
+      );
+    }
+
+    const notes = await this.noteRepository.find({
+      where: whereCondition,
+      order: { id: 'desc' },
+    });
+
     return notes.map((note: Note) => new NoteDto(note));
   }
 
-  async create(noteData: CreateNoteDto): Promise<number> {
-    const result = await this.noteRepository.insert(noteData);
-    return result.identifiers[0].id; // 삽입된 레코드의 ID 반환
+  async findById(userId: number, id: number): Promise<NoteDto> {
+    console.log(id);
+    const note = await this.noteRepository.findOne({ where: { id, userId } });
+
+    if (!note) {
+      throw new NotFoundException('Note not found');
+    }
+
+    return new NoteDto(note);
+  }
+
+  async create(noteData: CreateNoteDto, userId: number): Promise<NoteDto> {
+    const data = {
+      userId,
+      title: noteData.title,
+      content: noteData.content,
+    };
+
+    const note = this.noteRepository.create(data);
+
+    const result = await this.noteRepository.save(note);
+    return new NoteDto(result);
+  }
+
+  async update(
+    id: number,
+    noteData: UpdateNoteDto,
+    userId: number,
+  ): Promise<NoteDto> {
+    const note = await this.noteRepository.findOne({ where: { id, userId } });
+
+    if (!note) {
+      throw new NotFoundException('Note not found');
+    }
+
+    note.title = noteData.title;
+    note.content = noteData.content;
+
+    const result = await this.noteRepository.save(note);
+    return new NoteDto(result);
+  }
+
+  async delete(id: number, userId: number): Promise<number> {
+    const note = await this.noteRepository.findOne({ where: { id, userId } });
+
+    if (!note) {
+      throw new NotFoundException('Note not found');
+    }
+
+    await this.noteRepository.softDelete(id);
+    return id;
   }
 }
