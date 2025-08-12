8337375af36d84f1fbe5eda0dbcd3aec5f94e403
seonghong1 <wodeanniversary@naver.com>
2025-08-12 22:29:24 +0900

    getUtcStartAndEndDates 수정

--- src/common/data.util.ts
+++ src/common/data.util.ts
@@ -5,27 +5,23 @@ export function getUtcStartAndEndDates(timestamp: number): {
   const date = new Date(timestamp);
 
   const startDate = new Date(
-    Date.UTC(
-      date.getUTCFullYear(),
-      date.getUTCMonth(),
-      date.getUTCDate(),
-      0,
-      0,
-      0,
-      0,
-    ),
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    0,
+    0,
+    0,
+    0,
   );
 
   const endDate = new Date(
-    Date.UTC(
-      date.getUTCFullYear(),
-      date.getUTCMonth(),
-      date.getUTCDate(),
-      23,
-      59,
-      59,
-      999,
-    ),
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    23,
+    59,
+    59,
+    999,
   );
 
   return { startDate, endDate };
