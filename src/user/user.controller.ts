d01e4250fe21562cd2a03d48c3f6a935796737b3
seonghong1 <wodeanniversary@naver.com>
2025-08-15 14:22:54 +0900

    회원가입 controller @Public 추기

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,14 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
 import { UserDto } from './dto/user.dtd';
+import { Public } from 'src/auth/constants';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('/signin')
-  login(@Body() userData: LoginUserDto): Promise<LoginUserDto> {
-    // 실제로 jwt 토큰 발급 필요
-    return this.userService.login(userData);
-  }
-
+  @Public()
   @Post('/signup')
   signup(@Body() userData: CreateUserDto): Promise<UserDto> {
     return this.userService.create(userData);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,6 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
-import { LoginUserDto } from './dto/login-user.dto';
 import { UserDto } from './dto/user.dtd';
 
 @Injectable()
@@ -45,28 +44,6 @@ export class UserService {
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      throw new BadRequestException('비밀번호가 일치하지 않습니다.');
-    }
-
-    return user;
-  }
-
-  // 로그인
-  async login(userData: LoginUserDto): Promise<LoginUserDto> {
-    const user = await this.userRepository.findOne({
-      where: { email: userData.email },
-    });
-
-    if (!user) {
-      throw new BadRequestException('존재하지 않는 이메일입니다.');
-    }
-
-    const isPasswordValid = await bcrypt.compare(
-      userData.password,
-      user.password,
-    );
 
     if (!isPasswordValid) {
       throw new BadRequestException('비밀번호가 일치하지 않습니다.');
